7e81801d5e375ce380407e4b80bab27abcede876
Albert <afaceisnomore@gmail.com>
2018-01-08 16:56:57 +0500

    add new resume file writing

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ fs.readdir(userParams.resumeDir, (error, files) => {
   files.slice(0, 1).map(filename => {
     if (utils.isResumeFile(filename)) {
       const fileData = fs.readFileSync(userParams.resumeDir + filename).toString()
+      const newFileDir = userParams.rewrite 
+        ? userParams.newResumeDir 
+        : userParams.resumeDir
+      fs.writeFileSync(
+        `${newFileDir}/${filename}`,
+        utils.parseResumeFileData(fileData)
+      )
     }
   })
 })
