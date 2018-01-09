da29d4e408ec25a5eb98b17cf582425b3e5bc76b
Albert <afaceisnomore@gmail.com>
2018-01-10 02:12:26 +0500

    add main function

--- src/main.js
+++ src/main.js
@@ -10,18 +10,24 @@ const userParams = {
   newResumeDir: '/home/aface/Programming/js/relocate-torrents/src/'
 }
 
-fs.readdir(userParams.resumeDir, (error, files) => {
-  if (error) { throw error }
-  files.map(filename => {
-    if (utils.isResumeFile(filename)) {
-      const fileData = fs.readFileSync(userParams.resumeDir + filename).toString()
-      const newFileDir = userParams.rewrite 
-        ? userParams.newResumeDir 
-        : userParams.resumeDir
-      fs.writeFileSync(
-        `${newFileDir}/${filename}`,
-        utils.parseResumeFileData(fileData, userParams)
-      )
-    }
+export const main = userParams => {
+  fs.readdir(userParams.resumeDir, (error, files) => {
+    if (error) { throw error }
+    files.map(filename => {
+      if (utils.isResumeFile(filename)) {
+        const fileData = fs.readFileSync(
+          userParams.resumeDir + filename
+        ).toString()
+        const newFileDir = userParams.rewrite 
+          ? userParams.newResumeDir 
+          : userParams.resumeDir
+        fs.writeFileSync(
+          `${newFileDir}/${filename}`,
+          utils.parseResumeFileData(fileData, userParams)
+        )
+      }
+    })
   })
-})
+}
+
+main(userParams)
